064deab76c29e4597ea50c10857c0ef8ee91bb48
kimarre <kimarrexo@gmail.com>
2015-09-30 00:23:13 -0700

    Malloc: no chunk, existing chunk, and no more room

--- assignment1/malloc.c
+++ assignment1/malloc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define CHUNK_SIZE = 65536;
-static break = NULL;
-static lastNode = NULL;
+#define CHUNK_SIZE 65536
+#define HEADER_SIZE 16
 
 typedef struct Block {
    struct Block *next;
@@ -11,20 +11,63 @@ typedef struct Block {
    int size;
 } Block;
 
+static int chunkRemaining = CHUNK_SIZE;
+static Block *lastBlock = NULL;
+
+void initNewBlock(Block *lastBlock, Block *prevBlock, int size) {
+   chunkRemaining -= size + sizeof(Block);
+
+   if (prevBlock) {
+      prevBlock->next = lastBlock;
+   } else {
+      prevBlock = NULL;
+   }
+
+   lastBlock->isTaken = 1;
+   lastBlock->size = size;
+}
+
 void *malloc(size_t size) {
-   // if: we've used sbrk before and have a chunk
-   //    if: there's a free already made block to use
-   //       use it
-   //    else: 
-   //       if: there's more room in the chunk
-   //          make a new block
-   //       else:
-   //          make a new chunk
-   //          make a new block
+   void *prevBlock = lastBlock;
+   // [x] if: we've used sbrk before and have a chunk
+   // [ ]    if: there's a free already made block to use
+   // [ ]       use it
+   // [ ]    else: 
+   // [x]       if: there's more room in the chunk
+   // [x]          make a new block
+   // [x]       else:
+   // [x]          make a new chunk
+   // [x] else:
+   // [x]    use sbrk to make our first chunk
+
+
+   if (!lastBlock) {
+      // A chunk doesn't exist yet
+      lastBlock = (Block *)sbrk((intptr_t)CHUNK_SIZE);
+
+      // Make the first block
+      initNewBlock(lastBlock, NULL, size);
+
+      return lastBlock + 1;
+   } else {
+      // ### do this later! if there's an already made block that's been freed
+
+      if (chunkRemaining < size + HEADER_SIZE) {
+         // if we need more chunk space
+         sbrk((intptr_t)CHUNK_SIZE);
+         chunkRemaining += CHUNK_SIZE;
+      }
+
+      Block *prevBlock = lastBlock;
+      lastBlock += lastBlock->size + 1;
+      initNewBlock(lastBlock, prevBlock, size);
+
+      return lastBlock + 1;
+   }
 
    char *str = "In the new malloc";
    int blockSize = sizeof(Block);
-   
+
    return 0;
 }
 
--- assignment1/testing.c
+++ assignment1/testing.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-   void *temp = malloc(16);
+typedef struct {
+   int num;
+   char *name;
+} Pet;
 
+int main() {
+   Pet *charlie = (Pet *)malloc(sizeof(Pet));
+
+   charlie->num = 2;
+   charlie->name = "Charlie-man";
+
+   int num = charlie->num;
+   char *name = charlie->name;
+
+
+   //second
+   Pet *moose = (Pet *)malloc(70000);
+   moose->num = 3;
+   moose->name = "Moose-cat";
+
+   int num2 = moose->num;
+   char *moosename = moose->name;
+   
    return 0;
 }
